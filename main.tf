9e4f370340db9ece85753f7711095defff23365c
Rangnath Jadhav <88224106+rjadhav7@users.noreply.github.com>
2021-10-18 17:39:24 +0530

    Update main.tf
    SL project-2 code updated

--- main.tf
+++ main.tf
@@ -1,78 +1,62 @@
-resource "aws_security_group" "jenkins-SG" { 
-  name = "jenkins-SG" 
-
-  ingress { 
-
-    from_port   = 8080 
-    to_port     = 8080 
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"] 
-
-  } 
-
-
-  ingress { 
-
-    from_port   = 22 
-    to_port     = 22 
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"] 
-
-  } 
-
-
-  egress { 
-
-    from_port   = 0 
-    to_port     = 0 
-    protocol    = "-1" 
-    cidr_blocks = ["0.0.0.0/0"] 
-
-  } 
-
-} 
-
-resource "aws_instance" "JenkinsEC2" { 
-
-  instance_type          = "t2.micro" 
-
-  ami                    = "ami-00e87074e52e6c9f9" 
-
-  vpc_security_group_ids = [aws_security_group.jenkins-SG.id] 
-
-  key_name               = "rj-key" 
-
-  tags = { 
-
-    Name = "Jenkins-Master" 
-
-  } 
- 
-provisioner "file" {
-    source      = "/home/project/script.sh"
-    destination = "/tmp/script.sh"
+resource "aws_security_group" "jenkins-SG" {
+  name = "jenkins-SG"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  # Change permissions on bash script and execute from ec2-user.
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "JenkinsEC2" {
+  instance_type = "t2.micro"
+  ami = "ami-00e87074e52e6c9f9"
+  vpc_security_group_ids = [aws_security_group.jenkins-SG.id]
+  key_name = "jenkins-key"
+  tags = {
+    Name = "Jenkins-Master"
+}
+##Bootstrap Master and Client for password letss authontications##
+  provisioner "file" {
+    source      = "/home/rjadhav155gmail/.ssh/id_rsa.pub"
+    destination = "/home/centos/.ssh/id_rsa.pub"
+  }
   provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/script.sh",
-      "sh /tmp/script.sh",
-    ]
+    inline = ["sudo cat /home/centos/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys"
+]
   }
-  
-  # Login to the ec2-user with the aws key.
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.JenkinsEC2.public_dns} > ./inventory.txt"
+  }
+
+  #Login to the centos user with the aws key.
   connection {
     type        = "ssh"
     user        = "centos"
     password    = ""
-    private_key = file("/home/project/rj-key.pem")
+    private_key = "${file(var.ssh_key_private)}"
     host        = self.public_ip
+  }
+  #Run Ansible Play book to install Jenkins, python3 & Java
+  provisioner "local-exec" {
+    command = "ansible-playbook installJenkins.yml -i inventory.txt"
   }
 
 }
 
-output "jenkins_endpoint" { 
+output "jenkins_endpoint" {
 
-  value = "${formatlist("http://%s:%s/", aws_instance.JenkinsEC2.*.public_ip, "8080")}" 
-
-  }
+  value = formatlist("http://%s:%s/", aws_instance.JenkinsEC2.*.public_ip, "8080")
+}
